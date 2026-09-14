3e12829a15b475256f80fab09ad4ee30529c36be
omar-misbar <omar@misbar-tech.com>
2026-09-15 01:14:34 +0300

    Validate activity mapping seed references

--- IXApi/src/Infrastructure/Persistence/Seeding/Processes/Process651/WfProcess651SeedData.ActivityMappingVariables.cs
+++ IXApi/src/Infrastructure/Persistence/Seeding/Processes/Process651/WfProcess651SeedData.ActivityMappingVariables.cs
@@ -18,13 +18,27 @@ public sealed partial class WfProcess651SeedData
             new WfActivityMappingVariable { RecId = 330L, ActivityControlId = 39450L, VariableId = 3630L, IsActive = true },
         };
 
-        foreach (var item in items)
-        {
-            if (!await db.Set<WfActivityMappingVariable>().IgnoreQueryFilters().AnyAsync(x => x.RecId == item.RecId, ct))
-            {
-                db.Set<WfActivityMappingVariable>().Add(item);
-            }
-        }
+        var existingMappingIds = await db.Set<WfActivityMappingVariable>().IgnoreQueryFilters()
+            .Where(x => items.Select(m => m.RecId).Contains(x.RecId))
+            .Select(x => x.RecId)
+            .ToListAsync(ct);
+
+        var activityControlIds = items.Select(x => x.ActivityControlId).Distinct().ToArray();
+        var variableIds = items.Select(x => x.VariableId).Distinct().ToArray();
+        var validActivityControlIds = await db.WfActivityControls.IgnoreQueryFilters()
+            .Where(x => activityControlIds.Contains(x.RecId))
+            .Select(x => x.RecId)
+            .ToHashSetAsync(ct);
+        var validVariableIds = await db.WfVariables.IgnoreQueryFilters()
+            .Where(x => variableIds.Contains(x.RecId))
+            .Select(x => x.RecId)
+            .ToHashSetAsync(ct);
+
+        db.Set<WfActivityMappingVariable>().AddRange(
+            items.Where(item =>
+                !existingMappingIds.Contains(item.RecId)
+                && validActivityControlIds.Contains(item.ActivityControlId)
+                && validVariableIds.Contains(item.VariableId)));
 
         await SaveWithIdentityAsync(db, "WfActivityMappingVariables", ct);
     }
