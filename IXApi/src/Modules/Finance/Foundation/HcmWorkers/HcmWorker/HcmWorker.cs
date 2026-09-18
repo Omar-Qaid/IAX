c4c9397f16992e9b532637e7cb924a4f4c0e467a
omar-misbar <omar@misbar-tech.com>
2026-09-18 04:33:54 +0300

    remove NotMapped Name and NameAlias from HcmWorker entity

--- IXApi/src/Modules/Finance/Foundation/HcmWorkers/HcmWorker/HcmWorker.cs
+++ IXApi/src/Modules/Finance/Foundation/HcmWorkers/HcmWorker/HcmWorker.cs
@@ -22,12 +22,6 @@ public class HcmWorker : Entity<long>
     public DateTime? BirthDate { get; set; }
     public string? UserId { get; set; }
 
-    [NotMapped]
-    public string Name { get; set; } = string.Empty;
-
-    [NotMapped]
-    public string NameAlias { get; set; } = string.Empty;
-
     public virtual DirPartyTable Party { get; set; } = null!;
     public virtual Occupation Occupation { get; set; } = null!;
     public virtual Gender Gender { get; set; } = null!;
