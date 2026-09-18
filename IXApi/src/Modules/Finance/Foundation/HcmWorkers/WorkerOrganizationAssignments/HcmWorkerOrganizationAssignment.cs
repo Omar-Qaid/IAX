1c9f7e6fd57609a786ed9598dee1b42028d5d22f
omar-misbar <omar@misbar-tech.com>
2026-09-18 04:46:15 +0300

    change HcmWorkerOrganizationAssignment base class to Entity<long>

--- IXApi/src/Modules/Finance/Foundation/HcmWorkers/WorkerOrganizationAssignments/HcmWorkerOrganizationAssignment.cs
+++ IXApi/src/Modules/Finance/Foundation/HcmWorkers/WorkerOrganizationAssignments/HcmWorkerOrganizationAssignment.cs
@@ -1,17 +1,18 @@
+using IAX.IXApi.Modules.Finance.Entities;
 using IAX.IXApi.Modules.Finance.Foundation.HcmWorkers;
 using IAX.IXApi.Modules.Finance.Foundation.OrganizationUnits;
 using IAX.IXApi.Modules.Finance.Foundation.Structure;
 
 namespace IAX.IXApi.Modules.Finance.Foundation.WorkerOrganizationAssignments;
 
-public class HcmWorkerOrganizationAssignment : IMultiCompany
+public class HcmWorkerOrganizationAssignment : Entity<long>
 {
     public long AssignmentId { get; set; }
     public string DataAreaId { get; set; } = "dat";
     public long HcmWorkerId { get; set; }
     public long OrganizationUnitId { get; set; }
     public long? PositionId { get; set; }
-    public long OrganizationRoleId { get; set; }
+    public long? OrganizationRoleId { get; set; }
     public byte AssignmentRole { get; set; }
     public DateOnly ValidFrom { get; set; }
     public DateOnly? ValidTo { get; set; }
@@ -21,5 +22,5 @@ public class HcmWorkerOrganizationAssignment : IMultiCompany
     public virtual HcmWorker HcmWorker { get; set; } = null!;
     public virtual OrganizationUnit OrganizationUnit { get; set; } = null!;
     public virtual HcmPosition? Position { get; set; }
-    public virtual OrganizationRole OrganizationRole { get; set; } = null!;
+    public virtual OrganizationRole? OrganizationRole { get; set; }
 }
